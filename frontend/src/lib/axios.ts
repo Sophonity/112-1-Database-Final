870115f837a56afccb1bed026514a7506bb70a48
Yen-Chun (Andrew) Huang <86509252+Sophonity@users.noreply.github.com>
2024-04-12 23:42:30 +0800

    Revert to origin

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,16 +1,7 @@
 import axios from 'axios';
-import { string } from 'zod';
-
-const backend_URL_dict: {[id: string]: string} = {
-  "lab_computer": "140.112.20.130:8080/api",
-  "lab_server": "",
-  "vercel": "",
-  "local": "http://localhost:8080/api"
-};
 
 export const url =
-  // process.env.NODE_ENV === 'production' ? 'https://api.example.com' : 'http://localhost:8080/api';
-  backend_URL_dict[process.env.BACKEND_TYPE || "local"];
+  process.env.NODE_ENV === 'production' ? 'https://api.example.com' : 'http://localhost:8080/api';
 
 const instance = axios.create({
   baseURL: url,
