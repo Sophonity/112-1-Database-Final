547a9d2cea0ada04092c71062086620c57263825
Yen-Chun (Andrew) Huang <86509252+Sophonity@users.noreply.github.com>
2024-04-20 00:51:34 +0800

    Deploy: update backend URL @ frontend

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -8,7 +8,8 @@ import axios from 'axios';
 // };
 
 export const url =
-  process.env.NODE_ENV === 'production' ? 'https://api.example.com' : 'http://localhost:8080/api';
+  // process.env.NODE_ENV === 'production' ? 'https://api.example.com' : 'http://localhost:8080/api';
+  process.env.BACKEND_URL || 'http://localhost:8080/api';
 
 const instance = axios.create({
   baseURL: url,
