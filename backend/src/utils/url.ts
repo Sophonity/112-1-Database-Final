b562281309de233dee358484c3d609f351128939
Yen-Chun (Andrew) Huang <86509252+Sophonity@users.noreply.github.com>
2024-04-20 10:19:09 +0800

    Deploy: update URL info @ backend

--- backend/src/utils/url.ts
+++ backend/src/utils/url.ts
@@ -1,5 +1,7 @@
 export const backendurl =
-  process.env.NODE_ENV === 'production' ? 'https://api.example.com' : 'http://localhost:8080/api';
+  // process.env.NODE_ENV === 'production' ? 'https://api.example.com' : 'http://localhost:8080/api';
+  process.env.BACKEND_URL || 'http://localhost:8080/api';
 
 export const frontendUrl =
-  process.env.NODE_ENV === 'production' ? 'https://example.com' : 'http://localhost:3000';
+  // process.env.NODE_ENV === 'production' ? 'https://example.com' : 'http://localhost:3000';
+  process.env.FRONTEND_URL || 'http://localhost:3000';
